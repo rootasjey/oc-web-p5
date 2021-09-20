7e9ff80b190f659d99a58093e84065cba33fd6c0
jeremservier <jeremie.coprinot.creme-de-la-creme@servier.com>
2021-09-20 22:09:09 +0200

    refactor: :recycle: rename variable [item] → [produit]

--- views/index.js
+++ views/index.js
@@ -6,15 +6,15 @@ function main() {
   .then(function (data) {
     const container = document.getElementById("container")
 
-    for (const item of data) {
+    for (const produit of data) {
       const domItem = document.createElement('div')
       const domItemName = document.createElement('div')
       const domItemPrice = document.createElement('div')
       const domItemImg = document.createElement('img')
 
-      domItemName.textContent = item.name
-      domItemPrice.textContent = item.price
-      domItemImg.src = item.imageUrl
+      domItemName.textContent = produit.name
+      domItemPrice.textContent = produit.price
+      domItemImg.src = produit.imageUrl
 
       domItemImg.width = 80
       domItemImg.height = 80
@@ -24,9 +24,9 @@ function main() {
       domItem.appendChild(domItemPrice)
 
       domItem.addEventListener('click', () => {
-        window.location = `produit.html?id=${item._id}`
+        window.location = `produit.html?id=${produit._id}`
       })
-
+      
       container.appendChild(domItem)
     }
   })
