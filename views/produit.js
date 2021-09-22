8b1b5a9f665449e5149285bfb3c5fc6945dd02d2
Nina <morville5audrey@gmail.com>
2021-09-23 01:33:52 +0200

    managing product page with js and css

--- views/produit.js
+++ views/produit.js
@@ -4,10 +4,9 @@ const urlSearchParam = new URLSearchParams(window.location.search)
 const params = Object.fromEntries(urlSearchParam.entries() )
 console.log(params)
 
+
 //appelle à la fonction principale qui définie les query strings et recupere un teddy par son id avec le .id qui fait reference au parametre dans l'url
 get(params.id)
-
-
 
 //Déclaration de la fonction permettant l'appel a l'api pour l'afichage de ses données
 function get(teddy) {
@@ -16,33 +15,78 @@ function get(teddy) {
   //Gestion de la promesse envoyé par l'api
   .then ( (resp)=>resp.json() )
   .then ( (data) =>{
-    console.log(teddy)
+    //console.log(teddy)
+    console.log(data)
     const Container = document.getElementById('container')
 
     const domItem = document.createElement('div')
     const domItemTitle = document.createElement('h1')
+    const domItemImgBox = document.createElement('div')
+    const domItemInfos = document.createElement('div')
+    const domItemAdd = document.createElement('button')
+
+    
+    const domItemImg = document.createElement('img')
     const domItemName = document.createElement('div')
     const domItemPrice = document.createElement('div')
-    const domItemImg = document.createElement('img')
 
+
+    domItem.classList.add('card')
     domItemTitle.textContent = "Produit"
-    domItemName.textContent = teddy.name
-    domItemPrice.textContent = teddy.price
-    domItemImg.src = teddy.imageUrl
-
+    domItemTitle.classList.add('title')
+    domItemImgBox.classList.add('img-card')
+    domItemImg.src = data.imageUrl
+    domItemImg.classList.add('img')
     domItemImg.width = 300
     domItemImg.height = 300
+    domItemImg.setAttribute('alt', 'Teddy\'s picture')
+    domItemInfos.classList.add('infos-card')
+    domItemAdd.classList.add('ajout')
+    domItemAdd.textContent = "Ajouter au panier"
+
+    domItemName.textContent = data.name
+    domItemName.classList.add('name')
+    domItemPrice.textContent = data.price /100+ '$'
+    domItemPrice.classList.add('price')
+
+
+    const domItemDropdown = document.createElement('select')
+    domItemDropdown.name = 'colors'
+    domItemDropdown.name = 'colors-select'
+    domItemDropdown.classList.add('color-select')
+
+    for (const color of data.colors) {
+        //console.log(color)
+        const option = document.createElement('option')
+        option.classList.add('color-option')
+        option.value = color
+        option.text = color
+        domItemDropdown.appendChild(option)
+    }
+
+
+    domItemImgBox.appendChild(domItemImg)
+    domItemInfos.appendChild(domItemName)
+    domItemInfos.appendChild(domItemPrice)
+    domItemInfos.appendChild(domItemDropdown)
+  
+
 
     domItem.appendChild(domItemTitle)
-    domItem.appendChild(domItemImg)
-    domItem.appendChild(domItemName)
-    domItem.appendChild(domItemPrice)
+    domItem.appendChild(domItemImgBox)
+    domItem.appendChild(domItemInfos)
+    domItem.appendChild(domItemAdd)
 
     Container.appendChild(domItem)
-   
+
+    
+    domItemAdd.addEventListener( 'click', ()=>{
+      window.location = `panier.html`
+    })
   })
   
 }
+
 
 
 
