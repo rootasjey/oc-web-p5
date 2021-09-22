62fec573db97e8fa3dd0937aa2dacd3007b1b952
Nina <morville5audrey@gmail.com>
2021-09-22 19:20:52 +0200

    creation of the dom elements with javascript

--- views/produit.js
+++ views/produit.js
@@ -1,24 +1,48 @@
-const domCardTeddy = window.location.search
-const params = 
+
+//definition des query params pour manipuler les données de l'element au click
+const urlSearchParam = new URLSearchParams(window.location.search)
+const params = Object.fromEntries(urlSearchParam.entries() )
+console.log(params)
+
+//appelle à la fonction principale qui définie les query strings et recupere un teddy par son id avec le .id qui fait reference au parametre dans l'url
+get(params.id)
 
 
 
-function get(teddyId) {
-  fetch(`http://localhost:2021/api/teddies/${teddyId}`)
-  // fetch(`http://localhost:2021/api/teddies/5be9c8541c9d440000665243`)
+//Déclaration de la fonction permettant l'appel a l'api pour l'afichage de ses données
+function get(teddy) {
+  //appelle a l'api
+  fetch(`http://localhost:2021/api/teddies/${teddy}`)
+  //Gestion de la promesse envoyé par l'api
   .then ( (resp)=>resp.json() )
   .then ( (data) =>{
+    console.log(teddy)
+    const Container = document.getElementById('container')
 
-    for (const teddyId of data) {
-      const domItem = document.createElement('div')
-      const domItemName = document.createElement('div')
-      const domItemPrice = document.createElement('div')
-      const domItemImg = document.createElement('img')
-    }
+    const domItem = document.createElement('div')
+    const domItemTitle = document.createElement('h1')
+    const domItemName = document.createElement('div')
+    const domItemPrice = document.createElement('div')
+    const domItemImg = document.createElement('img')
+
+    domItemTitle.textContent = "Produit"
+    domItemName.textContent = teddy.name
+    domItemPrice.textContent = teddy.price
+    domItemImg.src = teddy.imageUrl
+
+    domItemImg.width = 300
+    domItemImg.height = 300
+
+    domItem.appendChild(domItemTitle)
+    domItem.appendChild(domItemImg)
+    domItem.appendChild(domItemName)
+    domItem.appendChild(domItemPrice)
+
+    Container.appendChild(domItem)
+   
   })
   
 }
-get(domCardTeddy)
 
 
 
