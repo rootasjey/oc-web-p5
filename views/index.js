7c9f2af805a9fcf42cbde2545758eb18248b1bd8
Nina <morville5audrey@gmail.com>
2021-09-23 23:04:37 +0200

    add elements and style to index page

--- views/index.js
+++ views/index.js
@@ -5,29 +5,45 @@ function main() {
   .then((response) => response.json())
   .then(function (data) {
     const container = document.getElementById("container")
+    const domItemBox = document.createElement('div')
+    
+
+    domItemBox.classList.add('box')
 
     for (const produit of data) {
       const domItem = document.createElement('div')
-      const domItemName = document.createElement('div')
-      const domItemPrice = document.createElement('div')
+      domItem.classList.add('card')
+
       const domItemImg = document.createElement('img')
 
-      domItemName.textContent = produit.name
-      domItemPrice.textContent = produit.price
-      domItemImg.src = produit.imageUrl
+      const domItemInfos = document.createElement('div')
+      domItemInfos.classList.add('infos-card')
 
+      const domItemName = document.createElement('div')
+      domItemName.classList.add('info-name')
+      const domItemPrice = document.createElement('div')
+      domItemPrice.classList.add('info-price')
+
+      domItemImg.src = produit.imageUrl
       domItemImg.width = 80
       domItemImg.height = 80
+      domItemName.textContent = produit.name
+      domItemPrice.textContent = produit.price/100 +"$"
+
+      domItemInfos.appendChild(domItemName)
+      domItemInfos.appendChild(domItemPrice)
 
       domItem.appendChild(domItemImg)
-      domItem.appendChild(domItemName)
-      domItem.appendChild(domItemPrice)
+      domItem.appendChild(domItemInfos)
+
+      domItemBox.appendChild(domItem)
 
       domItem.addEventListener('click', () => {
         window.location = `produit.html?id=${produit._id}`
       })
       
-      container.appendChild(domItem)
+      container.classList.add('container')
+      container.appendChild(domItemBox)
     }
   })
 }
